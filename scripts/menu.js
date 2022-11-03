6d22a0534e6d6ac1c2182be394cab2b4b036e8c1
Jagneel <jagneel@hotmail.com>
2022-11-03 20:49:11 +0000

    smoother menu animations

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,17 @@
 const menuItems = document.querySelectorAll('details')
+const menuItemFigure = document.querySelectorAll('details figure')
 
 for(let i=0; i<menuItems.length; i++){
     menuItems[i].addEventListener('mouseenter', function() {
-        menuItems[i].setAttribute('open', true)       
+        menuItems[i].setAttribute('open', true)   
+        console.log(menuItemFigure[i])    
     })
     menuItems[i].addEventListener('mouseleave', function() {
-        menuItems[i].removeAttribute('open')
+        menuItemFigure[i].setAttribute('style', 'animation: sweep-out 0.4s ease-out;')
+        setTimeout(function() {
+            menuItems[i].removeAttribute('open')
+            menuItemFigure[i].removeAttribute('style', 'animation: sweep-out 0.4s ease-out;')
+        }, 300)    
     })
 }
+
